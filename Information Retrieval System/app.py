aeaa196623a7dbb1111505b5f8ce904b5222276d
unknown <yazid.ben-madani@epita.fr>
2024-07-29 21:33:45 +0200

    create streamlit header

---
+++ Information Retrieval System/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+
+def main():
+
+    st.set_page_config("Information Retrieval")
+    st.header("Information Retrieval System ")
+
+if __name__ == "__main__":
+    main()
