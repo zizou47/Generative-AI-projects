20d253b2a7a7a2315c2d94abec0a6b193e6c4b4e
unknown <yazid.ben-madani@epita.fr>
2024-07-29 18:07:57 +0200

    install requirements, get api

---
+++ Information Retrieval System/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+
+
+    name= "Generative AI project",
+    version="0.0.0",
+    author= "yazid",
+    author_email="yazid.ben-madani@epita.fr",
+    packages= find_packages(),
+    install_requires = []
+)
